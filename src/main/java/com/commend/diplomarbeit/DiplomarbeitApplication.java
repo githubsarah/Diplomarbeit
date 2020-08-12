defcdce8f09a62ef44c3520bd9c03f00f3140ad9
s.Schwaiger <s.schwaiger@commend.com>
2020-08-12 07:08:07 +0200

    add Rooms

--- src/main/java/com/commend/diplomarbeit/DiplomarbeitApplication.java
+++ src/main/java/com/commend/diplomarbeit/DiplomarbeitApplication.java
@@ -1,23 +1,18 @@
 package com.commend.diplomarbeit;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.jdbc.core.JdbcTemplate;
 
 @SpringBootApplication
-public class DiplomarbeitApplication implements CommandLineRunner {
+public class DiplomarbeitApplication{
 
-    private static final Logger log = LoggerFactory.getLogger(DiplomarbeitApplication.class);
+   // private static final Logger log = LoggerFactory.getLogger(DiplomarbeitApplication.class);
 
     public static void main(String[] args) {
         SpringApplication.run(DiplomarbeitApplication.class, args);
     }
 
-    @Autowired
+   /* @Autowired
     JdbcTemplate jdbcTemplate;
 
     @Override
@@ -30,5 +25,5 @@ public class DiplomarbeitApplication implements CommandLineRunner {
                 "number SERIAL, id SERIAL, patientid SERIAL, fn VARCHAR(255), ln VARCHAR(255), content VARCHAR(255))");
 
         jdbcTemplate.batchUpdate("INSERT INTO rooms(number, id, patientid, fn, ln, content) VALUES (?,?,?,?,?,?)");
-    }
+    }*/
 }
