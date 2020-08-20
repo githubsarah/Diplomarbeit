084b109791530519c31cd259ee616397711e5e2f
s.Schwaiger <s.schwaiger@commend.com>
2020-08-20 06:44:23 +0200

    Refactor Patient Package

--- src/main/java/com/commend/diplomarbeit/DiplomarbeitApplication.java
+++ src/main/java/com/commend/diplomarbeit/DiplomarbeitApplication.java
@@ -2,23 +2,12 @@ package com.commend.diplomarbeit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-//  import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
-@SwaggerEnable2
 public class DiplomarbeitApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DiplomarbeitApplication.class, args);
     }
 
-    /*
-    @Bean
-    public Docket productApi() {
-        return new Docket(DocumentationType.SWAGGER_2).select()
-                .apis(RequestHandlerSelectors.basePackage("com.commend.diplomarbeit")).build();
-    }
-
-     */
 }
--- src/main/java/com/commend/diplomarbeit/SwaggerEnable2.java
+++
@@ -1,6 +0,0 @@
-package com.commend.diplomarbeit;
-
-import org.springframework.context.annotation.Bean;
-
-public @interface SwaggerEnable2 {
-}
