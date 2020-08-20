24e3de5fca4c0c52f878a2805e4f3c972c3e0647
s.Schwaiger <s.schwaiger@commend.com>
2020-08-20 12:49:06 +0200

    Add SwaggerEnable2

--- src/main/java/com/commend/diplomarbeit/DiplomarbeitApplication.java
+++ src/main/java/com/commend/diplomarbeit/DiplomarbeitApplication.java
@@ -1,9 +1,12 @@
 package com.commend.diplomarbeit;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -20,8 +23,21 @@ public class DiplomarbeitApplication {
     @Bean
     public Docket productApi() {
         return new Docket(DocumentationType.SWAGGER_2).select()
-                .apis(RequestHandlerSelectors.basePackage("com.commend.diplomarbeit")).build();
+                .apis(RequestHandlerSelectors.basePackage("com.commend.diplomarbeit")).paths(PathSelectors.any()).build()
+                .apiInfo(metaData());
     }
 
+    private ApiInfo metaData() {
+        ApiInfo apiInfo = new ApiInfo(
+                "Spring Boot REST API",
+                "Spring Boot REST API for Diplomarbeit",
+                "1.0",
+                "Terms of service",
+                "Sarah Schwaiger",
+                "Apache License Version 2.0",
+                "https://www.apache.org/licenses/LICENSE-2.0");
+
+        return apiInfo;
+    }
 
 }
